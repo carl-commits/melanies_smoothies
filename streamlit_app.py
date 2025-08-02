913f83707e9752e32a908d0d37da2d126b9ffefb
carl-commits <lueck.carl@gmail.com>
2025-08-02 11:41:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert snowpark df to pandas df
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop
+# st.dataframe(pd_df)
+# st.stop
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
@@ -36,7 +36,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
 
