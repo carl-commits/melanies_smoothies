f7fc4fbb82218be52345ff4e040a7473f37c8b7b
carl-commits <lueck.carl@gmail.com>
2025-08-02 11:08:11 +0200

    Update streamlit_app.py
    added response to fruit_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    # st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """' , '""" + name_on_order + """')"""
@@ -41,5 +42,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
+
